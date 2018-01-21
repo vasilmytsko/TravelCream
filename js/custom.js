384e1a862bbf3560bffa254a74f943b5137a9ec5
vasil <vasilmytsko@gmail.com>
2018-01-21 07:19:27 +0200

    add files

---
+++ js/custom.js
@@ -0,0 +1,13 @@
+/*placeholder*/
+$(function () {
+    $('input, textarea').each(function () {
+        var placeholder = $(this).attr('placeholder');
+        $(this).focus(function () {
+            $(this).attr('placeholder', '');
+        });
+        $(this).focusout(function () {
+            $(this).attr('placeholder', placeholder);
+        });
+    });
+});
+
